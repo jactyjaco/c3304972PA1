6267f9402e0377a0d3957395abf5fda38afbb168
Jacob Tye <jacob.tye@uon.edu.au>
2019-04-03 07:21:11 +0000

    Written a rotational encryption block of code but am struggling with negative keys? Special characters are replacing shift letters and whilst this would be a great encryption method decryption will be made near impossible.
    CASE EXAMPLE: Key = 1. Message = 'Attack' ---- Expected output = 'Zsszbj' ----- Programs actual output = '@ss'bj'
    
    
    Will have to research the issue

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <strings.h>
 
 int main(){
-    printf("Welcome");
+    char message[] = "Attack", ch;
+    int key = -1, cout = 0;
+    printf("Welcome to the C encryptor/decryptor\n-------------------------------------\n");
+    printf("Message: %s\n", message);
+    printf("Rotational Shift = %d\n", key);
+    
+        for(cout = 0; message[cout] != '0';++cout){
+            ch = message[cout];
+            
+            if(ch >= 'a' && ch <= 'z'){
+                ch = ch + key;
+                
+                if (ch > 'z'){
+                    ch = ch - 'z' + 'a' - 1;
+                }
+                message[cout] = ch;
+            }
+
+            else if (ch >= 'A' && ch <= 'Z'){
+                ch = ch + key;
+                
+                if (ch > 'Z'){
+                ch = ch - 'Z' + 'A' - 1; 
+                }
+                message[cout] = ch;
+            }
+    }
+    printf("Encrypted Message: %s\n", message);
     return 0;
 }
