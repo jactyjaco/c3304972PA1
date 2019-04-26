6f1d0a94031d0db47196db8620bab957b09c7d56
Jacob Tye <jacob.tye@uon.edu.au>
2019-04-26 05:25:35 +0000

    Started on user interface

--- main.c
+++ main.c
@@ -2,13 +2,27 @@
 #include <string.h>
 #include <strings.h>
 
+int rotationalEncrypt(char message[], int key);
+
 int main(){
-    char message[] = "attack", ch;
-    int key = 1, cout = 0;
-    printf("Welcome to the C encryptor/decryptor\n-------------------------------------\n");
-    printf("Message: %s\n", message);
-    printf("Rotational Shift = %d\n", key);
-    
+    char message[1000];
+    int key = 1;
+    printf("Please select a number from the menu below:\n-------------------------------------\n");
+    printf("1) Rotational Encryption\n");
+    printf("2) Rotational Decryption\n");
+    printf("3) Substitutional Encryption\n");
+    printf("4) Substitutional Decryption\n");
+    printf("To exit press 0\n-------------------------------------\n");
+    fgets(message,1000,stdin);
+    printf("Your message: %s\n", message);
+    printf("Rotational Shift = %d\n-------------------------------------\n", key);
+    rotationalEncrypt(message, key);
+    return 0;
+}
+
+int rotationalEncrypt(char message[], int key){
+        char ch;
+        int cout;
         for(cout = 0; message[cout] != '0';++cout){
             ch = message[cout];
             
