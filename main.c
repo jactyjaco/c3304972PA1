54e3e368311aa9d6fb4a9bc62ed35776ff63de98
Jacob Tye <jacob.tye@uon.edu.au>
2019-04-26 07:47:42 +0000

    Started rotational decrypt

--- main.c
+++ main.c
@@ -61,3 +61,33 @@ int rotationalEncrypt(char message[], int key){
     printf("Encrypted Message: %s\n", message);
     return 0;
 }
+
+int rotationalDecrypt(char message[], int key){
+    	for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch - key;
+			
+			if(ch < 'a'){
+				ch = ch + 'z' - 'a' + 1;
+			}
+			
+			message[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch - key;
+			
+			if(ch < 'A'){
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			
+			message[i] = ch;
+		}
+	}
+	
+	printf("Decrypted message: %s", message);
+	
+	return 0;
+}
+}
